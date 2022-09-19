577e6bbc21551ffbdae2683271ac1e1481599de3
Heidi <mcmarketing.ad@gmail.com>
2022-09-19 12:02:02 -0400

    format js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -24,11 +27,6 @@ function generatePassword() {
   var userWantLowercase = window.confirm("Would you like to include lowercase letters in your password? ");
   var userWantNumber = window.confirm("Would you like to include numbers in your password? ");
   var userWantSymbol = window.confirm("Would you like to include symbols in your password? ");
-
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var symbol = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
   var option = [];
 
